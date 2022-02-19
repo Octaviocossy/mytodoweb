e6cd76e5f1d4dca4d950b57373df505c83e59b50
OctavioCossy <octaviocossytorquati@gmail.com>
2022-02-19 13:22:38 -0300

    Configures eslint in the serverless

--- serverless/.eslintrc.js
+++ serverless/.eslintrc.js
@@ -1,15 +1,37 @@
 module.exports = {
   env: {
+    node: true,
     browser: true,
-    commonjs: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'standard'
-  ],
+  extends: ['standard', 'plugin:prettier/recommended'],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
   },
+  plugins: ['prettier'],
   rules: {
-  }
-}
+    'no-console': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        bracketSpacing: true,
+        arrowParens: 'always',
+        trailingComma: 'es5',
+        singleQuote: true,
+        endOfLine: 'auto',
+        tabWidth: 2,
+        semi: true,
+      },
+    ],
+    'padding-line-between-statements': [
+      'warn',
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var'],
+      },
+    ],
+  },
+};
